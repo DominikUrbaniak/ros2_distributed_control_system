8af6da19dce07fe05524cde54af55e97aef7126b
DominikUrbaniak <126696875+DominikUrbaniak@users.noreply.github.com>
2024-04-12 10:56:37 +0200

    Add files via upload

---
+++ distributed_control_experiments/experiment_pkg/run_aruco_test_1uc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+timeout 70 ros2 run experiment_pkg a1_image_publisher Sensor 9 30 4
+
+timeout 70 ros2 run experiment_pkg a1_image_publisher Sensor 9 30 3
+
+timeout 70 ros2 run experiment_pkg a1_image_publisher Sensor 9 30 2
+
+timeout 70 ros2 run experiment_pkg a1_image_publisher Sensor 9 30 1
+
+timeout 70 ros2 run experiment_pkg a1_image_publisher Sensor 9 30 0
---
+++ distributed_control_experiments/experiment_pkg/run_aruco_test_3uc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ros2 run experiment_pkg a3_pose_subscriber Sensor upc_laptop 4 3 uc 1 60
+
+sleep 10
+
+ros2 run experiment_pkg a3_pose_subscriber Sensor upc_laptop 3 3 uc 1 60
+
+sleep 10
+
+ros2 run experiment_pkg a3_pose_subscriber Sensor upc_laptop 2 3 uc 1 60
+
+sleep 10
+
+ros2 run experiment_pkg a3_pose_subscriber Sensor upc_laptop 1 3 uc 1 60
+
+sleep 10
+
+ros2 run experiment_pkg a3_pose_subscriber Sensor upc_laptop 0 3 uc 1 60
