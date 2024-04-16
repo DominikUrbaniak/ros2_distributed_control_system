a1733b42b910b33a98c8da4f650b6b77a4dc1705
DominikUrbaniak <126696875+DominikUrbaniak@users.noreply.github.com>
2024-04-16 10:30:13 +0200

    Delete qos_communication_tests/qos_pkg directory

--- qos_communication_tests/qos_pkg/run_a2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-timeout 405900 ros2 run qos_pkg poses_a2_sub_pub
-
-sleep 100
-
-timeout 405900 ros2 run qos_pkg poses_a2_sub_pub
--- qos_communication_tests/qos_pkg/run_a2_test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-timeout 38 ros2 run qos_pkg poses_a2_sub_pub
-
-sleep 2
-
-timeout 38 ros2 run qos_pkg poses_a2_sub_pub
